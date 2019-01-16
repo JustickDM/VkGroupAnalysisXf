31a865ca63a6e24541f72d7f70aeb21eda0e788d
IgoR-NiK <Igor_2501@mail.ru>
2019-01-16 23:39:06 +0300

    Добавлена базовая ViewModel

---
+++ MvvmAqua.Xamarin/ViewModels/BaseVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using MvvmAqua.Xamarin.Navigation;
+
+namespace MvvmAqua.Xamarin.ViewModels
+{
+	public abstract class BaseVM : ExtendedBindableObject
+	{
+		private INavigationService navigationService;
+		protected internal INavigationService NavigationService
+		{
+			get => navigationService;
+			internal set
+			{
+				navigationService = value;
+				NavigationServiceInitialization();
+			}
+		}
+
+		protected virtual void NavigationServiceInitialization() { }
+	}
+}
